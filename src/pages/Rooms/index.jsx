e4c0af7d462feca78e1fbd9e54430632d681eb3d
paulords95 <paulords95@gmail.com>
2020-10-15 16:29:51 -0300

    added rooms list

--- src/pages/Rooms/index.jsx
+++ src/pages/Rooms/index.jsx
@@ -11,7 +11,14 @@ const Rooms = () => {
   return (
     <div className="page-wrap">
       <h1 className="page-title-rooms">Dev Chat</h1>
-      <div className="chat-wrap"></div>
+      <div className="chat-wrap">
+        <ul className="rooms-list">
+          <li>Geral</li>
+          <li>Front End</li>
+          <li>Back End</li>
+        </ul>
+        <div className="chat-box"></div>
+      </div>
     </div>
   );
 };
